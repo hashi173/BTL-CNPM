a26f65e3e6acb3c4396325183999a705b373760a
hashi <phamthithienha17032005@gmail.com>
2026-06-02 22:36:14 +0700

    Add JUnit and Hamcrest libraries to fix test compilation for everyone

--- coffeeshop/build.bat
+++ coffeeshop/build.bat
@@ -1,6 +1,6 @@
 @echo off
 dir /s /B src\*.java > sources.txt
-javac -encoding UTF-8 --module-path "lib/javafx-sdk-17.0.12/lib" --add-modules javafx.controls -cp "lib/postgresql-42.7.5.jar;lib/atlantafx-base-1.0.0.jar" -d bin @sources.txt
+javac -encoding UTF-8 --module-path "lib/javafx-sdk-17.0.12/lib" --add-modules javafx.controls -cp "lib/postgresql-42.7.5.jar;lib/atlantafx-base-1.0.0.jar;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" -d bin @sources.txt
 xcopy /E /Y /I src\com\coffeeshop\resources bin\com\coffeeshop\resources
 jar cfm CoffeeShop.jar Manifest.txt -C bin .
 echo Build completed!
--- coffeeshop/build.sh
+++ coffeeshop/build.sh
@@ -6,7 +6,7 @@ mkdir -p bin
 find src -name "*.java" > sources.txt
 
 # Compile using JavaFX SDK and other dependencies
-javac -encoding UTF-8 --module-path "lib/javafx-sdk-17.0.12/lib" --add-modules javafx.controls -cp "lib/postgresql-42.7.5.jar:lib/atlantafx-base-1.0.0.jar" -d bin @sources.txt
+javac -encoding UTF-8 --module-path "lib/javafx-sdk-17.0.12/lib" --add-modules javafx.controls -cp "lib/postgresql-42.7.5.jar:lib/atlantafx-base-1.0.0.jar:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar" -d bin @sources.txt
 
 # Copy resources to bin directory
 mkdir -p bin/com/coffeeshop/resources
